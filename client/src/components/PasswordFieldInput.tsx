5df00c8818554e3b0fde83b6c021b973f6735743
Alistair Abao <abaolester@gmail.com>
2026-03-03 23:30:18 +0100

    refactored password inputs to a new file

---
+++ client/src/components/PasswordFieldInput.tsx
@@ -0,0 +1,39 @@
+import { TextInput } from "@mantine/core";
+import { IconEyeFilled, IconEyeOff } from "@tabler/icons-react";
+import { useState } from "react";
+
+interface PropsType {
+  name: string;
+}
+
+function PasswordFieldInput({ name }: PropsType) {
+  const [isVisible, setIsVisible] = useState(true);
+  //   const [isVisible, setIsVisible] = useState(true);
+
+  const handlePwdClick = () => {
+    setIsVisible(!isVisible);
+  };
+  //   const handlePwdClick = () => {
+  //     setIsVisible(!isVisible);
+  //   };
+
+  //   const icon = <IconAt size={16} onClick={handlePwdClick} />;
+  const eyefilled = <IconEyeFilled size={16} onClick={handlePwdClick} />;
+  //   const eyefilled2 = <IconEyeFilled size={16} onClick={handlePwdClick2} />;
+  const eyeclosed = <IconEyeOff size={16} onClick={handlePwdClick} />;
+  //   const eyeclosed2 = <IconEyeOff size={16} onClick={handlePwdClick2} />;
+  return (
+    <>
+      <TextInput
+        label='Password'
+        placeholder='Input Password'
+        type={isVisible ? "password" : "text"}
+        rightSection={isVisible ? eyefilled : eyeclosed}
+        name={name}
+        required
+      />
+    </>
+  );
+}
+
+export default PasswordFieldInput;
