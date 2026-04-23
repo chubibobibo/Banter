2a262801eb5c5cc084bcd5b9f51b5a51acada617
Alistair Abao <abaolester@gmail.com>
2026-04-23 23:51:27 +0200

    fix the redirecting top login when logging off, fix the closing of modal when navigating to udpateUser

--- client/src/utils/logoutHandler.ts
+++ client/src/utils/logoutHandler.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
-import { redirect } from "@tanstack/react-router";
 import { toast } from "react-toastify";
 
-export const logoutHandler = async () => {
+interface logoutHandlerType {
+  // defines the shape or structure of the navigate function
+  navigate: (option: { to: string }) => void;
+}
+
+/** logoutHandler accepts the navigate props from UserProfileComponent. This allow the use of the useNavigate hook outside a function component. */
+export const logoutHandler = async ({ navigate }: logoutHandlerType) => {
   try {
     await axios.post("/api/auth/logout");
-    redirect({ to: "/login" });
     toast.success("User successfully logged out");
+    navigate({ to: "/login" });
   } catch (err) {
+    console.log(err);
     toast.error(`Something went wrong, Cannot log user out: ${err}`);
   }
 };
